27dcb3b209efe7e63dd7b92708654c32e3404367
Noah Day <noah.day@adelaide.edu.au>
2022-03-09 17:58:15 +1030

    Create open-nc.py

---
+++ open-nc.py
@@ -0,0 +1,4 @@
+import netCDF4 as nc
+fn = '/Volumes/NoahDay5TB/Gadi/JRA55-do-1-5-0/prra/gr/v20200916/prra_input4MIPs_atmosphericState_OMIP_MRI-JRA55-do-1-5-0_gr_196201010130-196212312230.nc'
+
+ds = nc.Dataset(fn)
